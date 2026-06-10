314d77ca53fcbe4fe85383765a1c56d08b5c29b7
andreastaad-bit <andreastaad@gmail.com>
2026-06-10 16:20:32 -0600

    Create templatesConfig.js

---
+++ focus-video-creator-player/src/templatesConfig.js
@@ -0,0 +1,26 @@
+export const TEMPLATES = {
+  lluvia: {
+    id: 'lluvia',
+    nombre: 'Lluvia Profunda',
+    claseCSS: 'bg-template-rain',
+    sonidoAgua: '/assets/sounds/rain.mp3',
+    frecuencia: '/assets/frequencies/174hz.mp3',
+    label: '174Hz - Alivio'
+  },
+  bosque: {
+    id: 'bosque',
+    nombre: 'Manantial Zen',
+    claseCSS: 'bg-template-forest',
+    sonidoAgua: '/assets/sounds/river.mp3',
+    frecuencia: '/assets/frequencies/432hz.mp3',
+    label: '432Hz - Naturaleza'
+  },
+  oceano: {
+    id: 'oceano',
+    nombre: 'Océano Alfa',
+    claseCSS: 'bg-template-ocean',
+    sonidoAgua: '/assets/sounds/ocean.mp3',
+    frecuencia: '/assets/frequencies/528hz.mp3',
+    label: '528Hz - Energía'
+  }
+};
